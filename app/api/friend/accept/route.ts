bc0cf97751e80baaafd173de831435f1720e97c3
shay213 <dawidpawelec@o2.pl>
2023-06-22 12:03:59 +0200

    Display chats in dashboard page with last message

--- app/api/friend/accept/route.ts
+++ app/api/friend/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { z } from "zod";
@@ -41,6 +43,9 @@ export async function POST(req: Request) {
         }
       );
     }
+
+    // notify added user
+    pusherServer.trigger(toPusherKey(`user:${id}:friends`), "new_friend", {});
 
     await db.sadd(`user:${session.user.id}:friends`, id);
     await db.sadd(`user:${id}:friends`, session.user.id);
--- app/components/SidebarChatList.tsx
+++ app/components/SidebarChatList.tsx
@@ -56,6 +56,9 @@ const SidebarChatList = ({ friends, sessionId }: SidebarChatListProps) => {
     return () => {
       pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:chats`));
       pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`));
+
+      pusherClient.unbind("new_message", chatHandler);
+      pusherClient.unbind("new_friend", newFriendHandler);
     };
   }, [sessionId, router, pathname]);
 
